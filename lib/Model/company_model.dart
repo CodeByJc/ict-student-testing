67ed4e49496d75533b5be4b9cdc6bf530e636c58
Hetansh shah <103094264+Hetansh20@users.noreply.github.com>
2025-09-15 08:15:12 +0530

    Company Filter Done

--- lib/Model/company_model.dart
+++ lib/Model/company_model.dart
@@ -1,35 +1,39 @@
-  class CompanyModel {
-    int companyId;
-    String companyName;
-    String companyType;
-    String companyWebsite;
-    String companyLinkedin;
+class CompanyModel {
+  int companyId;
+  String companyName;
+  String companyType;
+  List<String> companyDomain; // now a list
+  String companyWebsite;
+  String companyLinkedin;
 
-    CompanyModel({
-      required this.companyId,
-      required this.companyName,
-      required this.companyType,
-      required this.companyWebsite,
-      required this.companyLinkedin,
-    });
+  CompanyModel({
+    required this.companyId,
+    required this.companyName,
+    required this.companyType,
+    required this.companyDomain,
+    required this.companyWebsite,
+    required this.companyLinkedin,
+  });
 
-    factory CompanyModel.fromJson(Map<String, dynamic> json) {
-      return CompanyModel(
-        companyId: json['id'] ?? 0,
-        companyName: json['company_name'] ?? "",
-        companyType: json['company_type'] ?? "",
-        companyWebsite: json['company_website'] ?? "",
-        companyLinkedin: json['company_linkedin'] ?? "",
-      );
-    }
-
-    Map<String, dynamic> toJson() {
-      return {
-        'id': companyId,
-        'company_name': companyName,
-        'company_type': companyType,
-        'company_website': companyWebsite,
-        'company_linkedin': companyLinkedin,
-      };
-    }
+  factory CompanyModel.fromJson(Map<String, dynamic> json) {
+    return CompanyModel(
+      companyId: json['id'] ?? 0,
+      companyName: json['company_name'] ?? "",
+      companyType: json['company_type'] ?? "",
+      companyDomain: (json['company_domain'] as String?)?.split(",").map((e) => e.trim()).toList() ?? [],
+      companyWebsite: json['company_website'] ?? "",
+      companyLinkedin: json['company_linkedin'] ?? "",
+    );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': companyId,
+      'company_name': companyName,
+      'company_type': companyType,
+      'company_domain': companyDomain.join(", "),
+      'company_website': companyWebsite,
+      'company_linkedin': companyLinkedin,
+    };
+  }
+}
