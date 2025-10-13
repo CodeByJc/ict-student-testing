36cb6be9d93014995c29b95dd72d37b1311cfaff
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-10-13 17:09:15 +0530

    Add files via upload

---
+++ lib/Bindings/job_market_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import '../Controllers/job_market_controller.dart';
+import '../Controllers/internet_connectivity.dart';
+
+class JobMarketBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(InternetConnectivityController());
+    Get.put(JobMarketController());
+  }
+}
