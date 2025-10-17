b741a9257e35a87908ecfee3ffd2f0a32abc9b3c
Shivam Bhatt <143023404+Shivambhatt2305@users.noreply.github.com>
2025-10-17 12:48:19 +0530

    update ui with proper working
    Refactored the DashboardScreen layout, replacing the previous grid icon implementation with a new TapIcon widget. Updated the UI components for better aesthetics and functionality.

--- lib/Screens/Home/dashboard_home.dart
+++ lib/Screens/Home/dashboard_home.dart
@@ -7,12 +7,12 @@ import 'package:get_storage/get_storage.dart';
 import 'package:hugeicons/hugeicons.dart';
 import 'package:ict_mu_students/Helper/Components.dart';
 import 'package:ict_mu_students/Model/holiday_list_model.dart';
+import 'package:ict_mu_students/Widgets/dashboard_icon.dart';
 import '../../Helper/colors.dart';
 import '../../Model/user_data_model.dart';
 import '../../Network/API.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
-import '../CareerGuidance/career_guidance_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -41,248 +41,176 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF5F7FA),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: blackTag(
-              context,
-              muColor,
-              "${userData.studentDetails?.firstName} ${userData.studentDetails?.lastName}",
-              "Sem: ${userData.classDetails?.semester}  Class: ${userData.classDetails?.className} - ${userData.classDetails?.batch?.toUpperCase()}",
-              CachedNetworkImage(
-                imageUrl: studentImageAPI(userData.studentDetails!.grNo),
-                placeholder: (context, url) => const HugeIcon(
-                  icon: HugeIcons.strokeRoundedUser,
-                  size: 20,
-                  color: Colors.black87,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Dashboard",
+            style: TextStyle(
+                color: Colors.black, fontFamily: "mu_reg", fontSize: 23)),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            blackTag(
+                context,
+                muColor,
+                "${userData.studentDetails?.firstName} ${userData.studentDetails?.lastName}",
+                "Sem: ${userData.classDetails?.semester}  Class: ${userData.classDetails?.className} - ${userData.classDetails?.batch?.toUpperCase()}",
+                CachedNetworkImage(
+                  imageUrl: studentImageAPI(userData.studentDetails!.grNo),
+                  placeholder: (context, url) => HugeIcon(
+                    icon: HugeIcons.strokeRoundedUser,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                  errorWidget: (context, url, error) => HugeIcon(
+                    icon: HugeIcons.strokeRoundedUser,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                  fit: BoxFit.cover,
                 ),
-                errorWidget: (context, url, error) => const HugeIcon(
-                  icon: HugeIcons.strokeRoundedUser,
-                  size: 20,
-                  color: Colors.black87,
-                ),
-                fit: BoxFit.cover,
-              ),
-              true,
-              '/profile',
-              userData,
-            ),
-          ),
-          const SizedBox(height: 8),
-          Expanded(
-            child: Padding(
+                true,
+                '/profile',
+                userData),
+            const SizedBox(height: 8),
+            Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
                 crossAxisCount: 3,
-                mainAxisSpacing: 8,
-                crossAxisSpacing: 8,
+                mainAxisSpacing: 6,
+                crossAxisSpacing: 6,
                 childAspectRatio: 0.95,
-                padding: const EdgeInsets.all(8),
-                physics: const AlwaysScrollableScrollPhysics(),
-                shrinkWrap: false,
+                padding: const EdgeInsets.all(6),
                 children: [
-                  _buildGridIcon(
-                    name: "Attendance",
-                    iconData: HugeIcons.strokeRoundedDocumentValidation,
-                    route: "/attendance",
-                    routeArg: {
-                      'student_id': userData.studentDetails?.studentId
-                    },
-                  ),
-                  _buildGridIcon(
-                    name: "Timetable",
-                    iconData: HugeIcons.strokeRoundedCalendar02,
-                    route: "/studentTimetable",
-                    routeArg: {
-                      'student_id': userData.studentDetails?.studentId
-                    },
-                  ),
-                  _buildGridIcon(
-                    name: "Examination",
-                    iconData: HugeIcons.strokeRoundedDocumentValidation,
-                    route: "/examList",
-                    routeArg: {
-                      'student_id': userData.studentDetails?.studentId
-                    },
-                  ),
-                  _buildGridIcon(
-                    name: "Holidays",
-                    iconData: HugeIcons.strokeRoundedSun01,
-                    route: "/holidayList",
-                  ),
-                  _buildGridIcon(
-                    name: "Placements",
-                    iconData: HugeIcons.strokeRoundedGraduationScroll,
-                    route: "/placements",
-                    routeArg: {
-                      'student_id': userData.studentDetails?.studentId,
-                      'batch_id': userData.studentDetails?.batchId
-                    },
-                  ),
-                  _buildGridIcon(
-                    name: "Leave",
-                    iconData: HugeIcons.strokeRoundedMessageUser01,
-                    route: "/leave",
-                    routeArg: {
-                      'student_id': userData.studentDetails?.studentId
-                    },
-                  ),
-                  _buildGridIcon(
+                  TapIcon(
+                      name: "Attendance",
+                      iconData: HugeIcons.strokeRoundedDocumentValidation,
+                      route: "/attendance",
+                      routeArg: {
+                        'student_id': userData.studentDetails?.studentId
+                      }),
+                  TapIcon(
+                      name: "Timetable",
+                      iconData: HugeIcons.strokeRoundedCalendar02,
+                      route: "/studentTimetable",
+                      routeArg: {
+                        'student_id': userData.studentDetails?.studentId
+                      }),
+                  TapIcon(
+                      name: "Examination",
+                      iconData: HugeIcons.strokeRoundedDocumentValidation,
+                      route: "/examList",
+                      routeArg: {
+                        'student_id': userData.studentDetails?.studentId
+                      }),
+                  const TapIcon(
+                      name: "Holidays",
+                      iconData: HugeIcons.strokeRoundedSun01,
+                      route: "/holidayList"),
+                  TapIcon(
+                      name: "Placements",
+                      iconData: HugeIcons.strokeRoundedGraduationScroll,
+                      route: "/placements",
+                      routeArg: {
+                        'student_id': userData.studentDetails?.studentId,
+                        'batch_id': userData.studentDetails?.batchId
+                      }),
+                  TapIcon(
+                      name: "Leave",
+                      iconData: HugeIcons.strokeRoundedMessageUser01,
+                      route: "/leave",
+                      routeArg: {
+                        'student_id': userData.studentDetails?.studentId
+                      }),
+                  const TapIcon(
                     name: "Events",
                     iconData: HugeIcons.strokeRoundedRanking,
                     route: "/events",
                   ),
-                  _buildGridIcon(
-                    name: "Anonymous Feedback",
-                    iconData: HugeIcons.strokeRoundedBubbleChatSecure,
-                    route: "/feedback",
-                    routeArg: {
-                      'student_id': userData.studentDetails?.studentId,
-                      'sem_id': userData.classDetails?.semId
-                    },
-                  ),
-                  _buildGridIcon(
-                    name: "Career Guidance",
-                    iconData: HugeIcons.strokeRoundedGraduationScroll,
-                    route: "/careerGuidance",
-                  ),
-                  _buildGridIcon(
-                    name: "Live Job Market",
-                    iconData: HugeIcons.strokeRoundedRanking,
-                    route: "/liveJobMarket",
-                  ),
+                  TapIcon(
+                      name: "Anonymous Feedback",
+                      iconData: HugeIcons.strokeRoundedBubbleChatSecure,
+                      route: "/feedback",
+                      routeArg: {'student_id': 52, 'sem_id': 7}),
+                  const TapIcon(
+                      name: "Career Guidance",
+                      iconData: HugeIcons.strokeRoundedGraduationScroll,
+                      route: "/careerGuidance"),
+                  const TapIcon(
+                      name: "Live Job Market",
+                      iconData: HugeIcons.strokeRoundedRanking,
+                      route: "/JobMarket"),
+                  TapIcon(
+                      name: "Announcements",
+                      iconData: HugeIcons.strokeRoundedMegaphone01,
+                      route: "/announcement",
+                      routeArg: {'batch_id': 1}),
                 ],
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: InkWell(
+            const SizedBox(height: 20),
+            InkWell(
               onTap: () => Get.toNamed("/holidayList"),
-              borderRadius: BorderRadius.circular(16),
-              child: Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
+              highlightColor: backgroundColor,
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
                   borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.08),
+                      offset: const Offset(0, 6),
+                      blurRadius: 16,
+                    )
+                  ],
                 ),
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(color: const Color(0xFFE0E0E0)),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 14, 16, 14),
+                  child: Row(
+                    children: [
+                      HugeIcon(
+                        icon: HugeIcons.strokeRoundedSun01,
+                        size: 28,
+                        color: muColor,
+                      ),
+                      const SizedBox(width: 12),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
                           children: [
-                            const Icon(
-                              HugeIcons.strokeRoundedSun01,
-                              color: Color(0xFF0098B5),
-                              size: 20,
-                            ),
-                            const SizedBox(width: 8),
                             const Text(
                               "Upcoming Holiday",
                               style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black87,
-                                fontFamily: "mu_reg",
-                              ),
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: 'mu_bold'),
+                            ),
+                            Text(
+                              upcomingHoliday.holidayDate.isNotEmpty
+                                  ? "${DateFormat('dd-MM-yyyy').format(DateTime.parse(upcomingHoliday.holidayDate))} - ${upcomingHoliday.holidayName}"
+                                  : "No upcoming holidays",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: muGrey3,
+                                  fontFamily: 'mu_reg'),
                             ),
                           ],
                         ),
-                        const SizedBox(height: 8),
-                        if (upcomingHoliday.holidayDate.isNotEmpty)
-                          Text(
-                            "${DateFormat('dd-MM-yyyy').format(DateTime.parse(upcomingHoliday.holidayDate))} - ${upcomingHoliday.holidayName}",
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Color(0xFF0098B5),
-                              fontFamily: "mu_reg",
-                            ),
-                          )
-                        else
-                          const Text(
-                            "No upcoming holidays",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey,
-                              fontFamily: "mu_reg",
-                            ),
-                          ),
-                      ],
-                    ),
+                      )
+                    ],
                   ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildGridIcon({
-    required String name,
-    required IconData iconData,
-    required String route,
-    Map<String, dynamic>? routeArg,
-  }) {
-    return GestureDetector(
-      onTap: () {
-        if (route == "/careerGuidance") {
-          Get.to(() => const CareerGuidanceScreen());
-        } else {
-          Get.toNamed(route, arguments: routeArg);
-        }
-      },
-      child: Card(
-        elevation: 3,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
-            gradient: const LinearGradient(
-              colors: [Color(0xFFE3F2FD), Colors.white],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              HugeIcon(
-                icon: iconData,
-                size: 28, // Further decreased for better fit
-                color: const Color(0xFF0098B5),
-              ),
-              const SizedBox(height: 6),
-              Flexible(
-                child: Text(
-                  name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                    fontFamily: "mu_reg",
-                  ),
-                ),
-              ),
-            ],
-          ),
+          ],
         ),
       ),
     );
